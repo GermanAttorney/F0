80ee0a1fc5579bed384e20bab442e869d6cc6492
GermanAttorney <69551994+GermanAttorney@users.noreply.github.com>
2020-08-12 20:51:20 -0500

    Create 03_Personal.info.py

---
+++ 03_Personal.info.py
@@ -0,0 +1,12 @@
+space=" "
+Nombre=input("Nombre:")
+Apellido=input("Apellido:")
+Localidad=input("Ubicacion:")
+Edad=input("Edad:")
+"""
+
+"""
+print("¿Usted es?"+space+Nombre+space+Apellido,
+      "¿Y donde estas?"+space+Localidad,
+      "¿Y hace cuanto me esperas?"+space+Edad
+      )
